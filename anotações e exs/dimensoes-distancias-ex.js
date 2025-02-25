5ec88d0658a0b741a8ac598b725a63730ca2584e
Adrian Silva <grsadrian01@outlook.com>
2025-02-25 09:40:31 -0300

    resolvi exs dimensões e distâncias

--- anotações e exs/dimensoes-distancias-ex.js
+++ anotações e exs/dimensoes-distancias-ex.js
@@ -1,12 +1,25 @@
 // Verifique a distância da primeira imagem em relação ao topo da página
-const img = document.querySelector('img')
- console.log(img.offsetWidth)
+const img = document.querySelector("img");
+console.log(img.offsetTop);
 
 // Retorne a soma da largura de todas as imagens
-// const imgs = document.querySelectorAll('img')
-// imgs.forEach((item, index) => {
-//     console.log(index, item.clientWidth)
-// })
+onload = () => {
+  const imgs = document.querySelectorAll("img");
+  let totalLargura = 0;
+  imgs.forEach((item) => {
+    totalLargura += item.offsetWidth;
+  });
+  console.log(totalLargura);
+};
 
-const imgRect = img.getBoundingClientRect();
-console.log(imgRect.top)
+// Verifique se os links da página possuem o mínimo recomendado para telas utilizadas com o dedo. (48px/48px de acordo com o Google)
+const links = document.querySelectorAll("a");
+links.forEach((item, index) => {
+  console.log(`${index + 1} ${item.offsetWidth} x ${item.offsetHeight}`);
+});
+
+// Se o browser for menor que 720px, adicione a classe menu-mobile ao menu
+const menu = document.querySelector(".menu");
+if (innerWidth < 720) {
+  menu.classList.add("menu-mobile");
+}
